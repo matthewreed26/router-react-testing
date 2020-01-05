1438565d0abf610729d8d617dec81318dc7991a5
Matt Reed <matthewreed26@gmail.com>
2020-01-05 15:24:08 -0500

    testing by converting app to compoenent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { pageText: 
+      `<p>
+      Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>` 
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div dangerouslySetInnerHTML={{__html: this.state.pageText}}></div>
+          <button onClick={() => this.setState({ pageText: 'Super Fun text' })}>Change Text</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
